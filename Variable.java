b6b578386e1a37b22fabf6f79e5268e3eb7f4a49
gayatrijagadale <gayatrijagadale025@gmail.com>
2026-02-01 14:23:11 +0530

    add var

---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable  {
+    
+    public static void main(String a[]){
+
+        int num1 = 3;   //variable
+        int num2 = 5;
+        int result = num1+num2;
+        System.out.println(result);
+    }
+}
