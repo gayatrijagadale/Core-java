94b04d4025a2e8f6c118999064ee4d4162902ec6
gayatrijagadale <gayatrijagadale025@gmail.com>
2026-02-01 15:28:53 +0530

    Datatypes

---
+++ DataType.java
@@ -0,0 +1,19 @@
+public interface DataType {
+    public static void main(String[] args) {
+        //primitive type
+        int num1  = 5;   //integer    -> 4 byte (32bits)
+        short sh = 223;   //short   -> 2 bytes
+        byte by = 127;  //byte     -> 1 bytes
+        long l = 7776434556l;//long  -> 8 bytes
+
+        float f = 7.9f;   //float    -> 4 byte
+        double d = 5.6666;  //double -> 8 bytes
+        
+
+        char c = 'k';    //char     ->2 bytes
+        
+        boolean  b = true;   //boolean -> 1bytes
+        
+        
+    }
+}
